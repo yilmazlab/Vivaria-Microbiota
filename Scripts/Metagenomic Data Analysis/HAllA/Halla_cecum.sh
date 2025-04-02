1955d8b0182306a77c4f9272144de106b70b67e0
Bahtiyar Yilmaz <bahtiyar.yilmaz.tr@gmail.com>
2025-04-02 10:19:06 +0200

    updated

--- Scripts/Metagenomic Data Analysis/HAllA/Halla_cecum.sh
+++ Scripts/Metagenomic Data Analysis/HAllA/Halla_cecum.sh
@@ -1,33 +1,45 @@
 #!/bin/sh
 #!/bin/bash
-#SBATCH --job-name="Halla2"
-#SBATCH --nodes=1
-#SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=96G
-#SBATCH --partition=bdw
 
-##SBATCH --output=/path/to/outfile
-##SBATCH --error=/path/to/errfile
+# SLURM job configuration
+#SBATCH --job-name="Halla2"          # Job name
+#SBATCH --nodes=1                    # Number of nodes
+#SBATCH --time=24:00:00              # Maximum runtime
+#SBATCH --mem-per-cpu=96G            # Memory per CPU
+#SBATCH --partition=bdw              # Partition to submit the job
+
+## Optional output and error file paths
+##SBATCH --output=/path/to/outfile    # Path to save standard output
+##SBATCH --error=/path/to/errfile     # Path to save standard error
 
 # For array jobs
-# Array job containing 100 tasks, run max 10 tasks at the same time
+# Array job containing 100 tasks, running a maximum of 10 tasks at the same time
 ##SBATCH --array=1-100%10
 
-#### Your shell commands below this line ####
+#### Shell commands start below ####
 
-## QIIME pipeline shell script ##
-
+# Set locale to avoid potential encoding issues
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-source activate halla
+# Create and activate a Python virtual environment
 python -m venv halla-env
 source halla-env/bin/activate
+
+# Install required Python packages
 pip install matplotlib fontTools
+
+# Navigate to the working directory containing input data
 cd "Library/CloudStorage/Dropbox/Ongoing Analysis/Wild-Type Mice vs Wild Mice Analysis/2022/Halla_cecum_metabolites_microbiota"
 
-halla -x cecum_microbiota.txt -y cecum_metabolites.txt -o Microbiota_vs_Metabolomics -m spearman --fdr_alpha 0.05 --fnr_thresh 0.2 --cbar_label 'Pairwise Spearman' 
+# Run HAllA analysis with specified parameters
+halla -x cecum_microbiota.txt -y cecum_metabolites.txt -o Microbiota_vs_Metabolomics \
+      -m spearman --fdr_alpha 0.05 --fnr_thresh 0.2 --cbar_label 'Pairwise Spearman'
+
+# Run HAllA diagnostics with 100 permutations
 hallagnostic -i Microbiota_vs_Metabolomics/ -n 100
+
+# Generate a hallagram visualization for the first dataset
 hallagram \
     -i Microbiota_vs_Metabolomics/ \
     --cbar_label 'Pairwise Spearman' \
@@ -36,7 +48,7 @@ hallagram \
     -n 50 \
     --block_border_width 1
 
-
+# Generate a hallagram visualization for the second dataset
 hallagram \
     -i Cecum_metabolites_microbiota/ \
     --cbar_label 'Pairwise Spearman' \
--- Scripts/Metagenomic Data Analysis/Kraken2/README.md
+++
@@ -1,1 +0,0 @@
-This folder contains the script for metagenomic analysis for plant and microbial members using Kraken2.
